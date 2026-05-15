6333e3041c5fd67611f38bc2239fa0c45ae3edee
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-05-15 00:18:38 -0700

    Update staging OTA link to Contact Dispatch release

--- src/constants/appLinks.ts
+++ src/constants/appLinks.ts
@@ -11,7 +11,7 @@ const IOS_PRODUCTION_UPDATE_URL =
 // Do not wrap them in exp+://expo-development-client or an encoded https URL here;
 // this website entry point is meant to hand off directly to Expo/Expo Go.
 const APP_ID = 'a3777b70-b973-4b3b-ba59-ed32bf5662e0'
-const GROUP_ID = 'b51c015a-f9d3-4b41-ad33-6cdbf21393df' // 2026-05-14: Payment Method Expansion + Ambulance Route Heading
+const GROUP_ID = '74e1684e-688a-4a89-bb71-6666c664fd16' // 2026-05-15: Contact Dispatch - Emergency Communication Room
 const ANDROID_PREVIEW_UPDATE_URL =
   `exp://u.expo.dev/${APP_ID}/group/${GROUP_ID}`;
 const IOS_PREVIEW_UPDATE_URL =
