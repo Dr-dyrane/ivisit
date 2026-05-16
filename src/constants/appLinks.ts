f90e9d895e4d2a469bdf588551a0761539d58d83
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-05-15 19:16:51 -0700

    Update staging OTA link to Contact Dispatch hardening release

--- src/constants/appLinks.ts
+++ src/constants/appLinks.ts
@@ -11,7 +11,7 @@ const IOS_PRODUCTION_UPDATE_URL =
 // Do not wrap them in exp+://expo-development-client or an encoded https URL here;
 // this website entry point is meant to hand off directly to Expo/Expo Go.
 const APP_ID = 'a3777b70-b973-4b3b-ba59-ed32bf5662e0'
-const GROUP_ID = '74e1684e-688a-4a89-bb71-6666c664fd16' // 2026-05-15: Contact Dispatch - Emergency Communication Room
+const GROUP_ID = '3b4f2f2e-12f0-47f1-859c-68f61af47da3' // 2026-05-15: Contact Dispatch hardening - fixes for service adapter, query keys, modal lifecycle, and UI cleanup
 const ANDROID_PREVIEW_UPDATE_URL =
   `exp://u.expo.dev/${APP_ID}/group/${GROUP_ID}`;
 const IOS_PREVIEW_UPDATE_URL =
