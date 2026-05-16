6b30b2e230b2c70a59b83462d30c671e6089b49c
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2026-05-15 22:24:53 -0700

    chore(links): update staging OTA group ID to 9a425bdc (2026-05-15)

--- src/constants/appLinks.ts
+++ src/constants/appLinks.ts
@@ -11,7 +11,7 @@ const IOS_PRODUCTION_UPDATE_URL =
 // Do not wrap them in exp+://expo-development-client or an encoded https URL here;
 // this website entry point is meant to hand off directly to Expo/Expo Go.
 const APP_ID = 'a3777b70-b973-4b3b-ba59-ed32bf5662e0'
-const GROUP_ID = '8d9fef0c-2c4d-43eb-b72c-ef4c261238fe' // 2026-05-15: Location hardening complete - all 5 passes LOC-1/2/3/4/6
+const GROUP_ID = '9a425bdc-c267-4d81-8bc3-a27222139084' // 2026-05-15: Preview - test external Expo Go access
 const ANDROID_PREVIEW_UPDATE_URL =
   `exp://u.expo.dev/${APP_ID}/group/${GROUP_ID}`;
 const IOS_PREVIEW_UPDATE_URL =
